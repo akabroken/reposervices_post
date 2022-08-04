ab396ad649238d051daefad759d449f465b1b01e
ken Maha <ken Maha>
2022-08-05 00:53:36 +0300

    abc123

--- src/main/java/com/repotech/reposervices/Reposervices.java
+++ src/main/java/com/repotech/reposervices/Reposervices.java
@@ -7,14 +7,20 @@ package com.repotech.reposervices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  *
  * @author amahayak
  */
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class,SqlInitializationAutoConfiguration.class})
+@ComponentScan(basePackages = "com.repotech.reposervices.*")
+@EntityScan("com.repotech.reposervices.*")
+@EnableJpaRepositories(basePackages = "com.repotech.repository.InvoiceRepository")
 public class Reposervices {
 
     public static void main(String... args) {
