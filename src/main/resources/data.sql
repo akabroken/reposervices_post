28431d6a2ce1eaf7f06ac8d83f4c812fccc2735b
ken Maha <ken Maha>
2022-08-04 22:23:31 +0300

    a112

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 
-insert into invoice(mw_sn, invoice_no) values("KRAMW00000000001","INVSN00000000001");
-insert into invoice(mw_sn, invoice_no) values("KRAMW00000000002","INVSN00000000002");
-insert into invoice(mw_sn, invoice_no) values("KRAMW00000000003","INVSN00000000003");
-insert into invoice(mw_sn, invoice_no) values("KRAMW00000000004","INVSN00000000004");
-insert into invoice(mw_sn, invoice_no) values("KRAMW00000000005","INVSN00000000005");
+-- insert into invoice(mw_sn, invoice_no) values("KRAMW00000000001","INVSN00000000001");
+-- insert into invoice(mw_sn, invoice_no) values("KRAMW00000000002","INVSN00000000002");
+-- insert into invoice(mw_sn, invoice_no) values("KRAMW00000000003","INVSN00000000003");
+-- insert into invoice(mw_sn, invoice_no) values("KRAMW00000000004","INVSN00000000004");
+-- insert into invoice(mw_sn, invoice_no) values("KRAMW00000000005","INVSN00000000005");
