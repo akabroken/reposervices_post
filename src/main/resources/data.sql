105a1bad145682052cca1f8ce8071a9a26a23316
ken Maha <ken Maha>
2022-08-05 13:27:01 +0300

    test component38

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-
--- insert into invoice(mw_sn, invoice_no) values("KRAMW00000000001","INVSN00000000001");
--- insert into invoice(mw_sn, invoice_no) values("KRAMW00000000002","INVSN00000000002");
--- insert into invoice(mw_sn, invoice_no) values("KRAMW00000000003","INVSN00000000003");
--- insert into invoice(mw_sn, invoice_no) values("KRAMW00000000004","INVSN00000000004");
--- insert into invoice(mw_sn, invoice_no) values("KRAMW00000000005","INVSN00000000005");
