bcb3d6173ceb61abfb216eb7b7a7a867944eeb9b
ken Maha <ken Maha>
2022-08-05 11:34:36 +0300

    test component2

--- src/main/java/com/repotech/reposervices/Reposervices.java
+++ src/main/java/com/repotech/reposervices/Reposervices.java
@@ -18,9 +18,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @author amahayak
  */
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class,SqlInitializationAutoConfiguration.class})
-@ComponentScan(basePackages = "com.repotech.reposervices")
+@ComponentScan(basePackages = "com.repotech.reposervices.InvoiceRepository")
 @EntityScan("com.repotech.reposervices.*")
-@EnableJpaRepositories(basePackages = "com.repotech.repository.InvoiceRepository")
+@EnableJpaRepositories(basePackages = "com.repotech.repository")
 public class Reposervices {
 
     public static void main(String... args) {
